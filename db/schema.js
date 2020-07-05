dbadc46a87cbab9008457182dac0bfdab514c88c
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-07-05 18:00:32 +0200

    introduction

---
+++ db/schema.js
@@ -0,0 +1,20 @@
+const { gql } = require("apollo-server");
+
+// Schema
+const typeDefs = gql`
+  type Curso {
+    titulo: String
+  }
+  type Tecnologia {
+    tecnologia: String
+  }
+  input CursoInput {
+    tecnologia: String
+  }
+  type Query {
+    obtenerCursos(input: CursoInput!): [Curso]
+    obtenerTecnologia: [Tecnologia]
+  }
+`;
+
+module.exports = typeDefs;
---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./db/schema");
+const resolvers = require("./db/resolvers");
+
+// servidor
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    const miContext = "Hola";
+    return {
+      miContext,
+    };
+  },
+});
+
+// arrancar el servidor
+server.listen().then(({ url }) => {
+  console.log(`Servidor listo en la URL ${url}`);
+});
