4e2ec7bc0a2682dc91827c34861ae259f9c31978
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-07-08 16:16:29 +0200

    Creación de usuarios

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,7 +1,32 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+
 // Resolvers
 const resolvers = {
   Query: {
-    obtenerCurso: () => "algo",
+    obtenerCurso: () => 'algo',
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input || {};
+
+      // Revisar si el usuario ya está registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error('El usuario ya está registrado');
+      }
+      // Hashear su password
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      try {
+        // Guardarlo en la base de datos
+        const usuario = new Usuario(input);
+        usuario.save();
+        return usuario;
+      } catch (error) {}
+      console.error(error);
+    },
   },
 };
 
--- db/schema.js
+++ db/schema.js
@@ -1,9 +1,28 @@
-const { gql } = require("apollo-server");
+const { gql } = require('apollo-server');
 
 // Schema
 const typeDefs = gql`
+  type Usuario {
+    id: ID
+    nombre: String
+    apellido: String
+    email: String
+    creado: String
+  }
+
+  input UsuarioInput {
+    nombre: String!
+    apellido: String!
+    email: String!
+    password: String!
+  }
+
   type Query {
     obtenerCurso: String
+  }
+
+  type Mutation {
+    nuevoUsuario(input: UsuarioInput): Usuario
   }
 `;
 
