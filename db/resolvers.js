c5dff64703ec36191c02928adfde934239370a8a
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-07-11 04:13:29 +0200

    autenticacion

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,12 +1,25 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+
+const crearToken = (usuario, secreta, expiresIn) => {
+  // console.log(usuario);
+  const { id, email, nombre, apellido } = usuario;
+  return jwt.sign({ id, email, nombre, apellido }, secreta, { expiresIn });
+};
 
 // Resolvers
 const resolvers = {
   Query: {
-    obtenerCurso: () => 'algo',
+    // (parent, args, context, info)
+    obtenerUsuario: async (_, { token }) => {
+      const usuarioId = await jwt.verify(token, process.env.SECRETA);
+      return usuarioId;
+    },
   },
   Mutation: {
+    // (parent, args, context, info)
     nuevoUsuario: async (_, { input }) => {
       const { email, password } = input || {};
 
@@ -26,6 +39,28 @@ const resolvers = {
         return usuario;
       } catch (error) {}
       console.error(error);
+    },
+    // (parent, args, context, info)
+    autenticarUsuario: async (_, { input }) => {
+      const { email, password } = input;
+      // Si el usuario existe
+      const existeUsuario = await Usuario.findOne({ email });
+      if (!existeUsuario) {
+        throw new Error('El usuario no existe');
+      }
+
+      // Revisar si el password es correcto
+      const passwordCorrecto = await bcryptjs.compare(
+        password,
+        existeUsuario.password
+      );
+      if (!passwordCorrecto) {
+        throw new Error('El Password es Incorrecto');
+      }
+      // Crear el token
+      return {
+        token: crearToken(existeUsuario, process.env.SECRETA, '24h'),
+      };
     },
   },
 };
